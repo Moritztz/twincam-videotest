bd881bcf0b645760dcab9ec864081add23113d9b
Moritztz <tm.283wings@gmail.com>
2018-12-17 16:47:55 +0900

    f

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let mediaRecorder = null;
 let chunks = [];    // 録画でデータを保持する
 let rcvStream = null;
 
+let timer;
+
 let videoTrack;
 let capabilities;
 let constraints;
@@ -221,22 +223,16 @@ $('#rcvrecstart').click(function () {
 function recStart(stream) {
     if (stream != null) {
         mediaRecorder = new MediaRecorder(stream); //録画用のインスタンス作成
-
         // 一定間隔で録画が区切られて、データが渡される
         mediaRecorder.ondataavailable = function(evt) {
             chunks.push(evt.data);
         }
-
         mediaRecorder.start(1000); //録画開始 1000ms 毎に録画データを区切る
         $('#console').text("recieved video recorder started");
     }
 }
 
 $('#recstop').click(function () {
-    if (mediaRecorder != null) {
-        mediaRecorder.stop();                        //録画停止
-        $('#console').text("recorder stopped");
-    }
     mediaRecorder.onstop = function (e) {
         //保存用URLの生成
         let videoBrob = new Blob([e.data], { type: e.data.type });
@@ -245,7 +241,146 @@ $('#recstop').click(function () {
         anchor.download = 'recorded.webm';
         anchor.href = window.URL.createObjectURL(videoBrob);
     }
+    if (mediaRecorder != null) {
+        mediaRecorder.stop();                        //録画停止
+        $('#console').text("recorder stopped");
+    }
 });
+
+//Statsボタン
+$('#getting-stats').on('click', () => {
+    timer = setInterval(() => {
+        getRTCStats(existingCall._negotiator._pc.getStats())
+    }, 1000);
+});
+
+$('#stop-acquiring-stats').on('click', () => {
+    clearInterval(timer);
+});
+
+async function getRTCStats(statsObject) {
+
+    //let trasportArray = [];
+    //let candidateArray = [];
+    //let candidatePairArray = [];
+    //let inboundRTPAudioStreamArray = [];
+    //let inboundRTPVideoStreamArray = [];
+    //let outboundRTPAudioStreamArray = [];
+    //let outboundRTPVideoStreamArray = [];
+    //let codecArray = [];
+    let mediaStreamTrack_senderArray = [];
+    let mediaStreamTrack_receiverArray = [];
+    //let mediaStreamTrack_local_audioArray = []
+    //let mediaStreamTrack_remote_audioArray = []
+    let mediaStreamTrack_local_videoArray = []
+    let mediaStreamTrack_remote_videoArray = []
+    //let candidatePairId = '';
+    //let localCandidateId = '';
+    //let remoteCandidateId = '';
+    //let localCandidate = {};
+    //let remoteCandidate = {};
+    //let inboundAudioCodec = {};
+    //let inboundVideoCodec = {};
+    //let outboundAudioCode = {};
+    //let outboundVideoCode = {};
+
+    let stats = await statsObject;
+    stats.forEach(stat => {
+        //if (stat.id.indexOf('RTCTransport') !== -1) {
+        //    trasportArray.push(stat);
+        //}
+        //if (stat.id.indexOf('RTCIceCandidatePair') !== -1) {
+        //    candidatePairArray.push(stat);
+        //}
+        //if (stat.id.indexOf('RTCIceCandidate_') !== -1) {
+        //    candidateArray.push(stat);
+        //}
+        //if (stat.id.indexOf('RTCInboundRTPAudioStream') !== -1) {
+        //    inboundRTPAudioStreamArray.push(stat);
+        //}
+        //if (stat.id.indexOf('RTCInboundRTPVideoStream') !== -1) {
+        //    inboundRTPVideoStreamArray.push(stat);
+        //}
+        //if (stat.id.indexOf('RTCOutboundRTPAudioStream') !== -1) {
+        //    outboundRTPAudioStreamArray.push(stat);
+        //}
+        //if (stat.id.indexOf('RTCOutboundRTPVideoStream') !== -1) {
+        //    outboundRTPVideoStreamArray.push(stat);
+        //}
+        if (stat.id.indexOf('RTCMediaStreamTrack_sender') !== -1) {
+            mediaStreamTrack_senderArray.push(stat);
+        }
+        if (stat.id.indexOf('RTCMediaStreamTrack_receiver') !== -1) {
+            mediaStreamTrack_receiverArray.push(stat);
+        }
+        //if (stat.id.indexOf('RTCCodec') !== -1) {
+        //    codecArray.push(stat);
+        //}
+    });
+
+    //trasportArray.forEach(transport => {
+    //    if (transport.dtlsState === 'connected') {
+    //        candidatePairId = transport.selectedCandidatePairId;
+    //    }
+    //});
+    //candidatePairArray.forEach(candidatePair => {
+    //    if (candidatePair.state === 'succeeded' && candidatePair.id === candidatePairId) {
+    //        localCandidateId = candidatePair.localCandidateId;
+    //        remoteCandidateId = candidatePair.remoteCandidateId;
+    //    }
+    //});
+    //candidateArray.forEach(candidate => {
+    //    if (candidate.id === localCandidateId) {
+    //        localCandidate = candidate;
+    //    }
+    //    if (candidate.id === remoteCandidateId) {
+    //        remoteCandidate = candidate;
+    //    }
+    //});
+
+    //inboundRTPAudioStreamArray.forEach(inboundRTPAudioStream => {
+    //    codecArray.forEach(codec => {
+    //        if (inboundRTPAudioStream.codecId === codec.id) {
+    //            inboundAudioCodec = codec;
+    //        }
+    //    });
+    //});
+    //inboundRTPVideoStreamArray.forEach(inboundRTPVideoStream => {
+    //    codecArray.forEach(codec => {
+    //        if (inboundRTPVideoStream.codecId === codec.id) {
+    //            inboundVideoCodec = codec;
+    //        }
+    //    });
+    //});
+    //outboundRTPAudioStreamArray.forEach(outboundRTPAudioStream => {
+    //    codecArray.forEach(codec => {
+    //        if (outboundRTPAudioStream.codecId === codec.id) {
+    //            outboundAudioCodec = codec;
+    //        }
+    //    });
+    //});
+    //outboundRTPVideoStreamArray.forEach(outboundRTPVideo => {
+    //    codecArray.forEach(codec => {
+    //        if (outboundRTPVideo.codecId === codec.id) {
+    //            outboundVideoCodec = codec;
+    //        }
+    //    });
+    //});
+    mediaStreamTrack_senderArray.forEach(mediaStreamTrack => {
+        if (mediaStreamTrack.kind === 'audio') {
+            //mediaStreamTrack_local_audioArray.push(mediaStreamTrack)
+        } else if (mediaStreamTrack.kind === 'video') {
+            mediaStreamTrack_local_videoArray.push(mediaStreamTrack)
+        }
+    });
+    mediaStreamTrack_receiverArray.forEach(mediaStreamTrack => {
+        if (mediaStreamTrack.kind === 'audio') {
+            //mediaStreamTrack_remote_audioArray.push(mediaStreamTrack)
+        } else if (mediaStreamTrack.kind === 'video') {
+            mediaStreamTrack_remote_videoArray.push(mediaStreamTrack)
+        }
+    });
+}
 
 //reloadボタン
 $('#reload').click(function () {
